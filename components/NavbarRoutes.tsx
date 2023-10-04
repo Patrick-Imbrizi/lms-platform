bb10e3046026216afc2d8453948797a57155021f
Patrick Imbrizi <patrick.imbrizi@gmail.com>
2023-10-04 14:26:52 -0300

    feat: auth and dynamic layouts created

---
+++ components/NavbarRoutes.tsx
@@ -0,0 +1,40 @@
+'use client';
+import { UserButton } from '@clerk/nextjs';
+import { LogOut } from 'lucide-react';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+import { Button } from './ui/button';
+import Link from 'next/link';
+
+const NavbarRoutes = () => {
+    const pathname = usePathname();
+
+
+    const isTeacherPage = pathname?.startsWith("/teacher");
+    const isPLayerPage = pathname?.includes("/chapter");
+
+    return (
+        <div className='flex gap-x-2 ml-auto'>
+            {isTeacherPage || isPLayerPage
+                ? (<Link href='/'>
+                    <Button size={"sm"} variant="ghost">
+                        <LogOut className='h-4 w-4 mr-2' />
+                        Exit
+                    </Button>
+                </Link>
+                )
+                : (
+                    <Link href='/teacher/courses'>
+                        <Button size={'sm'} variant="ghost">
+                            Teacher Mode
+                        </Button>
+                    </Link>
+                )}
+            <UserButton
+                afterSignOutUrl='/'
+            />
+        </div>
+    )
+}
+
+export default NavbarRoutes
