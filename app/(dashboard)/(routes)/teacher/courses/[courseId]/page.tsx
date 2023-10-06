b386c86e3974045001b114d375306b74e82857bd
Patrick Imbrizi <patrick.imbrizi@gmail.com>
2023-10-06 19:08:47 -0300

    feat: attachment form and delete function added

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,12 +3,13 @@ import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 import { IconBadge } from '@/components/IconBadge';
-import { CircleDollarSign, LayoutDashboard, ListChecks } from 'lucide-react';
+import { CircleDollarSign, File, LayoutDashboard, ListChecks } from 'lucide-react';
 import { TitleForm } from './_components/TitleForm';
 import { DescriptionForm } from './_components/DescriptionForm';
 import { ImageForm } from './_components/ImageForm';
 import { PriceForm } from './_components/PriceForm';
 import { CategoryForm } from './_components/CategoryForm';
+import { AttachmentForm } from './_components/AttachmentForm';
 
 const CourseIdPage = async ({
     params
@@ -24,7 +25,14 @@ const CourseIdPage = async ({
     const course = await db.course.findUnique({
         where: {
             id: params.courseId
-        }
+        },
+        include: {
+            attachments: {
+                orderBy: {
+                    createdAt: "desc"
+                },
+            },
+        },
     });
 
     const categories = await db.category.findMany({
@@ -111,6 +119,18 @@ const CourseIdPage = async ({
                             </h2>
                         </div>
                         <PriceForm
+                            initialData={course}
+                            courseId={course.id}
+                        />
+                    </div>
+                    <div>
+                        <div className='flex items-center gap-x-2'>
+                            <IconBadge icon={File} />
+                            <h2 className='text-xl'>
+                                Resources & Attachments
+                            </h2>
+                        </div>
+                        <AttachmentForm
                             initialData={course}
                             courseId={course.id}
                         />
