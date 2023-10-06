396cf04d98f1af3e9a2572e4f32699a86c8f83af
Patrick Imbrizi <patrick.imbrizi@gmail.com>
2023-10-06 18:16:06 -0300

    feat: price form and format

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,10 +3,11 @@ import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 import { IconBadge } from '@/components/IconBadge';
-import { LayoutDashboard } from 'lucide-react';
+import { CircleDollarSign, LayoutDashboard, ListChecks } from 'lucide-react';
 import { TitleForm } from './_components/TitleForm';
 import { DescriptionForm } from './_components/DescriptionForm';
 import { ImageForm } from './_components/ImageForm';
+import { PriceForm } from './_components/PriceForm';
 import { CategoryForm } from './_components/CategoryForm';
 
 const CourseIdPage = async ({
@@ -89,6 +90,31 @@ const CourseIdPage = async ({
                             value: category.id
                         }))}
                     />
+                </div>
+                <div className='space-y-6'>
+                    <div>
+                        <div className='flex items-center gap-x-2'>
+                            <IconBadge icon={ListChecks} />
+                            <h2 className='text-xl'>
+                                Course chapters
+                            </h2>
+                        </div>
+                        <div>
+                            TODO:Chapters
+                        </div>
+                    </div>
+                    <div>
+                        <div className='flex items-center gap-x-2'>
+                            <IconBadge icon={CircleDollarSign} />
+                            <h2 className='text-xl'>
+                                Sell your course
+                            </h2>
+                        </div>
+                        <PriceForm
+                            initialData={course}
+                            courseId={course.id}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
