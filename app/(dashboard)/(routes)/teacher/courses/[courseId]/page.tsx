657cdf02766143ed895d7f556a0a779626dda8af
Patrick Imbrizi <patrick.imbrizi@gmail.com>
2023-10-06 10:40:12 -0300

    feat: TitleForm component for updating course title

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -4,6 +4,7 @@ import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 import { IconBadge } from '@/components/IconBadge';
 import { LayoutDashboard } from 'lucide-react';
+import { TitleForm } from './_components/TitleForm';
 
 const CourseIdPage = async ({
     params
@@ -59,6 +60,10 @@ const CourseIdPage = async ({
                             Customize your course
                         </h2>
                     </div>
+                    <TitleForm
+                        initialData={course}
+                        courseId={course.id}
+                    />
                 </div>
             </div>
         </div>
