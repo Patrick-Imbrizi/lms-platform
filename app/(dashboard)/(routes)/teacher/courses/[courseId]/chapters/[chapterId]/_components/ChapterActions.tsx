28b88328807977f5538c379f12f1b4c2b1223e98
Patrick Imbrizi <patrick.imbrizi@gmail.com>
2023-10-08 23:15:13 -0300

    feat: publish and unpublish chapters feature added

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterActions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterActions.tsx
@@ -23,6 +23,26 @@ export const ChapterActions = ({
 }: ChapterActionsProps) => {
     const [isLoading, setIsLoading] = useState(false);
     const router = useRouter();
+
+    const onClick = async () => {
+        try {
+            setIsLoading(true);
+
+            if (isPublished) {
+                await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}/unpublish`);
+                toast.success("Chapter unpublished");
+            } else {
+                await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}/publish`);
+                toast.success("Chapter published");
+            }
+            router.refresh();
+        } catch (error) {
+            toast.error("Something went wrong");
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
     const onDelete = async () => {
         try {
             setIsLoading(true);
@@ -42,7 +62,7 @@ export const ChapterActions = ({
     return (
         <div className="flex items-center gap-x-2">
             <Button
-                onClick={() => { }}
+                onClick={onClick}
                 disabled={disabled || isLoading}
                 variant="outline"
                 size="sm"
